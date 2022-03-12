aef882252e40608af4afdb14bb7d96dff3a978e7
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2022-03-12 14:31:24 -0800

    Added TODO items

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ while True:
             break
         print("To manually kill the server, send another interrupt in the next 10 seconds.")
         sleep(10)
+
+# TODO:
+# * Add tracking for which users have websockets open
+# * Change buttons to progress bar?
+# * Group "assigned to others" commissions by artist
+# * Editable text fields
+# * Make newlines work in commission text fields somehow
+# * Figure out way to make multiple actions in a row bog things down less. Threading of sending websockets?
