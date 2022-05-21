44ad50b34b924d907c9c473445a2d2cce4633d73
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2022-05-21 10:43:26 -0700

    TODO Notes

--- main.py
+++ main.py
@@ -18,13 +18,10 @@ while True:
         sleep(10)
 
 # TODO:
-# * Finish commission confirmation
-# * Order commissions by when an artist accepts them
 # * Notes field for each commission? Editable on the web page
+# * Order commissions by when an artist accepts them
 # * Can view the history of each commissions
 # * Add tracking for which users have websockets open
 # * Figure out way to make multiple actions in a row bog things down less. Threading of sending websockets?
-# * Time counter of how long something has been in its current status
 # * Change buttons to progress bar?
-# * Change "User" role to "Artist" and get rid of "Is Artist"
 # * Look into splitting out commission updates from websockets
